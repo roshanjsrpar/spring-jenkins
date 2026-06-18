e5f987107872765c12a6b2742646123e7fae9a0f
kumariishruti0511-ai <kumariishruti0511@gmail.com>
2026-06-18 12:10:48 +0530

    first commit

---
+++ src/main/java/com/example/jenkins/SpringJenkinsApplication.java
@@ -0,0 +1,23 @@
+package com.example.jenkins;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJenkinsApplication {
+	
+	public static Logger logger= LoggerFactory.getLogger(SpringJenkinsApplication.class);
+	
+	public void init() {
+		logger.info("Apllication started...");
+	}
+
+	public static void main(String[] args) {
+		logger.info("Applicatin executed..");
+		SpringApplication.run(SpringJenkinsApplication.class, args);
+	}
+
+}
