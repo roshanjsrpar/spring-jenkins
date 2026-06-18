77c1a4b5ff0fec4db1be684daecc264a09c58c7d
kumariishruti0511-ai <kumariishruti0511@gmail.com>
2026-06-18 15:50:16 +0530

    remove statement

--- src/test/java/com/example/jenkins/SpringJenkinsApplicationTests.java
+++ src/test/java/com/example/jenkins/SpringJenkinsApplicationTests.java
@@ -15,7 +15,6 @@ class SpringJenkinsApplicationTests {
     @Test
     void contextLoads() {
         logger.info("Test case executing..");
-        logger.info("Test case executing secound..");
         assertEquals(true, true);
     }
 }
